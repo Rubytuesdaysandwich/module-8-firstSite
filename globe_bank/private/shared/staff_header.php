8966d5523aeecb709dc471282d4ea482c625e27d
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-08 12:51:49 -0600

    up

---
+++ globe_bank/private/shared/staff_header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GBI</title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+  </head>
+  <body>
+  <header> 
+      <h1>GBI staff Area</h1> 
+</header>   
+<navigation>
+    <ul>
+        <li> <a href="index.php">Menu</a></li>
+    </ul>
+</navigation>
