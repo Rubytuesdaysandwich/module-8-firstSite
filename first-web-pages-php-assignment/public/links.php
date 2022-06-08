aed1d729772de4e1104519beabf8b616541d4868
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-08 14:44:53 -0600

    comments

--- first-web-pages-php-assignment/public/links.php
+++ first-web-pages-php-assignment/public/links.php
@@ -12,11 +12,11 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        require_once('../private/initialize.php');
+        require_once('../private/initialize.php');//initialize file path
         
         ?>
-        <?php include('../private/shared/links_header/links_header.php');?>
-        <?php include('../private/shared/links_footer/links_footer.php');?>
+        <?php include('../private/shared/links_header/links_header.php');//links header path?>
+        <?php include('../private/shared/links_footer/links_footer.php');//links footer path?>
     </body>
 </html>
 
