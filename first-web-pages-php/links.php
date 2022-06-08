576d5e90e6773bdac95cbd479b004f2b78e76aeb
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-08 13:59:31 -0600

    first web pages php assignment

---
+++ first-web-pages-php/links.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <ul style="list-style: none;">
+            <li><a href="links2.php?page=1">Page 1</a></li>
+            <li><a href="links2.php?page=2">Page 2</a></li>
+            <li><a href="links2.php?page=3">Page 3</a></li>
+            <li><a href="links2.php?page=4">Page 4</a></li>
+            <li><a href="links2.php?page=5">Page 5</a></li>
+        </ul>
+    </body>
+</html>
