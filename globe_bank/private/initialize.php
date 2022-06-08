896fcd435dad2f97da5c96ef310e114432147e85
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-08 13:39:02 -0600

    last

--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -6,6 +6,6 @@
   define("PROJECT_PATH", dirname(PRIVATE_PATH));
   define("PUBLIC_PATH", PROJECT_PATH . '/public');
   define("SHARED_PATH", PRIVATE_PATH . '/shared');
-require_once('functions.php')
+require_once('functions.php')//getting the functions from the functions page
 
 ?>
