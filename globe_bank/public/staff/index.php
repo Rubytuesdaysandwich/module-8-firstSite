9eb7fd75f7b125dc2a16d65047c25e88ab12a1d9
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-08 13:24:30 -0600

    update paths for subjects.php

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,9 +1,9 @@
-<?php require_once('../../private/initialize.php');?><!--file path to initialize.php use static string not dynamic data-->
+<?php require_once('../../private/initialize.php');?><!--file path to initialize.php use static string not dynamic data still have to use the dot dots here to access the initialize php-->
 
 <?php $page_title='staff Menu';?><!--Page title-->
-<?php include('../../private/shared/staff_header.php');?><!--file path to private/shared/ staff_header-->
+<?php include(SHARED_PATH .'/staff_header.php');?><!--file path to private/shared/ staff_header-->
 <div id="content">
 
 </div>
 
-<?php include('../../private/shared/staff_footer.php');?><!--file path to private/shared/ staff_header-->
+<?php include(SHARED_PATH .'/staff_footer.php');?><!--file path to private/shared/ staff_header-->
--- globe_bank/public/staff/subjects/index.php
+++ globe_bank/public/staff/subjects/index.php
@@ -1,9 +1,9 @@
 <?php require_once('../../../private/initialize.php');?><!--file path to initialize.php use static string not dynamic data-->
 
 <?php $page_title='subjects';?>
-<?php include('../../../private/shared/staff_header.php');?><!--file path to private/shared/ staff_header-->
+<?php include(SHARED_PATH .'/staff_header.php');?><!--file path to private/shared/ staff_header-->
 <div id="content">
 
 </div>
 
-<?php include('../../../private/shared/staff_footer.php');?><!--file path to private/shared/staff_footer-->
+<?php include(SHARED_PATH .'/staff_footer.php');?><!--file path to private/shared/ staff_header-->
